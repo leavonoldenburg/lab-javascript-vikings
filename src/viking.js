8f58b34ca3441e0cf7a50b36db655f714cec71c6
Lea von Oldenburg <lea92.von.oldenburg@gmail.com>
2021-06-25 14:27:53 +0200

    iteration 1-3 complete

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,93 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
 
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
 
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack() {
+    let saxon = new Saxon(100, 8);
+    let viking = new Viking('Harald', 100, 120);
+    saxon.receiveDamage(viking.strength);
+    if (saxon.health < 0) {
+      this.saxonArmy.pop(saxon);
+    }
+    viking.receiveDamage(saxon.strength);
+
+    return saxon.receiveDamage(viking.strength);
+  }
+
+  saxonAttack() {
+    let saxon = new Saxon(100, 25);
+    let viking = new Viking('Harald', 100, 8);
+    viking.receiveDamage(saxon.strength);
+    if (viking.health < 0) {
+      this.vikingArmy.pop();
+    }
+    return viking.receiveDamage(saxon.strength);
+  }
+
+  showStatus() {}
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
